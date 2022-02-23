f8196d63459aba194577e1d6be5d84d76acc978b
GushikenKaito <kaito-gushiken19h@stu.kbc.ac.jp>
2022-02-23 18:14:14 +0900

    ステージ、スカイボックスの追加

---
+++ Assets/Scenes/ProtoMain/Scripts/EnemyMove.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour
+{
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    public GameObject target;private bool inArea = false;
+    public float chasespeed = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        GotoNextPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+
+        }
+
+        if (target.activeInHierarchy==false)
+        {
+            //GetComponent<Renderer>().material.color = origColor;
+        }
+
+        if (inArea==true&&target.activeInHierarchy==true)
+        {
+            agent.destination = target.transform.position;
+            EneChasing();
+        }
+    }
+
+    void GotoNextPoint()
+    {
+        if (points.Length == 0) return;
+
+        agent.destination = points[destPoint].position;
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            inArea = true;
+            target = other.gameObject;
+            EneChasing();
+            Destroy(gameObject);
+        }   
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inArea = false;
+            GotoNextPoint();
+        }
+    }
+
+    public void EneChasing()
+    {
+        transform.position += transform.forward * chasespeed;
+    }
+
+}
