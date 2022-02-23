fc240620638f40f3fa1afd7ea051a1f099cd1507
yumyum48 <puxipu939@gmail.com>
2022-02-23 16:55:26 +0900

    StageMgrを更新

--- Assets/Scenes/Stage/Scripts/Stage1Mgr.cs
+++ Assets/Scenes/Stage/Scripts/Stage1Mgr.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class Stage1Mgr : MonoBehaviour
 {
-    
+    int dieEnemyCount;  // 倒されたエネミーの数をカウント
+    [SerializeField]  int　dieEnemy_Max; // 倒せる敵の上限 
     // Start is called before the first frame update
     void Start()
     {
-        
+        dieEnemyCount = 0;  // 倒されたエネミーの数を初期化
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(dieEnemy_Max <= dieEnemyCount)   // ゲームクリア判定の確認
+        {
+            // GameClear
+            Debug.Log("GameClear");
+        }
+    }
+
+    // 倒されたエネミーの数を1増やす
+    public void DieEnemyCount()
+    {
+        dieEnemyCount++;
     }
 }
