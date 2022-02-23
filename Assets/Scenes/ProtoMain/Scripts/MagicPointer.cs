521ec858a4adc14041a20a157d98c638f873db84
Bologost <bio0730r@gmail.com>
2022-02-23 15:35:55 +0900

    やったらぁよ！

---
+++ Assets/Scenes/ProtoMain/Scripts/MagicPointer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicPointer : MonoBehaviour
+{
+    [SerializeField] GameObject me;
+    [SerializeField] GameObject floor;
+    [SerializeField] GameObject pointer;
+
+    Vector3 floorPos;
+    Vector3 floorNormal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        floorPos = floor.transform.position;
+        floorNormal = floor.transform.up;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //meの位置
+        Vector3 mePos = transform.position;
+
+        //meのベクトル
+        Vector3 meVec = transform.forward;
+
+        float h = Vector3.Dot(floorNormal, floorPos);
+
+        var point = mePos + ((h - Vector3.Dot(floorNormal,mePos)) / (Vector3.Dot(floorNormal, meVec))) * meVec;
+
+        pointer.transform.position = point;
+    }
+}
