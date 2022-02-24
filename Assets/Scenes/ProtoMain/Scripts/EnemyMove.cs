c9c9f7defb1469ab5f6c39ad26fe179aa9081eee
GushikenKaito <kaito-gushiken19h@stu.kbc.ac.jp>
2022-02-24 14:00:02 +0900

    敵のスポーン、動きを実装しました

--- Assets/Scenes/ProtoMain/Scripts/EnemyMove.cs
+++ Assets/Scenes/ProtoMain/Scripts/EnemyMove.cs
@@ -6,46 +6,90 @@ using UnityEngine.AI;
 public class EnemyMove : MonoBehaviour
 {
 
-    public Transform[] points;
+    public GameObject[] points=null;
     private int destPoint = 0;
     private NavMeshAgent agent;
-    public GameObject target;private bool inArea = false;
+   // public GameObject target;
+    private bool inArea = false;
     public float chasespeed = 0.05f;
+
+    private int ROOT;
+    private int RandR;
+    private bool RimitFlg = true;
+    public GameObject PrefabEnemy;
+    private GameObject CloneEnem;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        RandR = Random.Range(0, 3);
         agent = GetComponent<NavMeshAgent>();
         agent.autoBraking = false;
-        GotoNextPoint();
+        //GotoNextPoint();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (RimitFlg == true) {
+            
+       //     RimitFlg = false;
+      //  }
+
+        if (RandR==0)
+        {
+            for (int x = 0; x < points.Length; x++)//プレイヤー目線左方向のルートから敵が来る
+            {
+              //  if (points[x] == null)
+               // {
+                    points[x] = GameObject.Find("PointL" + x);
+               // }
+            }
+
+        }else if (RandR==1){
+            for (int x = 0; x < points.Length; x++)//プレイヤー目線真ん中方向のルートから敵が来る
+            {
+               // if (points[x] == null)
+               // {
+                    points[x] = GameObject.Find("PointM" + x);
+              //  }
+            }
+        }else if (RandR==2){
+            for (int x = 0; x < points.Length; x++)//プレイヤー目線右方向のルートから敵が来る
+            {
+             //   if (points[x] == null)
+              //  {
+                    points[x] = GameObject.Find("PointR" + x);
+             //   }
+            }
+
+        }
+
+
         if (agent.remainingDistance < 0.5f)
         {
             GotoNextPoint();
 
         }
 
-        if (target.activeInHierarchy==false)
-        {
-            //GetComponent<Renderer>().material.color = origColor;
-        }
+        //if (target.activeInHierarchy==false)
+        //{
+        //    //GetComponent<Renderer>().material.color = origColor;
+        //}
 
-        if (inArea==true&&target.activeInHierarchy==true)
-        {
-            agent.destination = target.transform.position;
-            EneChasing();
-        }
+        //if (inArea==true&&target.activeInHierarchy==true)
+        //{
+        //    agent.destination = target.transform.position;
+        //    EneChasing();
+        //}
     }
 
     void GotoNextPoint()
     {
-        if (points.Length == 0) return;
+        //if (points.Length == 0) return;
 
-        agent.destination = points[destPoint].position;
+        agent.destination = points[destPoint].transform.position;
         destPoint = (destPoint + 1) % points.Length;
     }
 
@@ -55,20 +99,31 @@ public class EnemyMove : MonoBehaviour
         {
 
             inArea = true;
-            target = other.gameObject;
+            //target = other.gameObject;
             EneChasing();
-            Destroy(gameObject);
-        }   
-    }
+            
+        }
 
-    void OnTriggerExit(Collider other)
-    {
         if (other.gameObject.tag == "Player")
         {
-            inArea = false;
-            GotoNextPoint();
+            Destroy(gameObject, 0.2f);
+            CloneEnem = Instantiate(PrefabEnemy, new Vector3(-0.71f, 0.69f, 26.02f), Quaternion.identity);
+            CloneEnem.name = "CloneEnemy";
         }
+
     }
+
+  
+
+
+    //void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Player")
+    //    {
+    //        inArea = false;
+    //        GotoNextPoint();
+    //    }
+    //}
 
     public void EneChasing()
     {
