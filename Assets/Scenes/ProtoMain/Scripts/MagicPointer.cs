08a7c907a0cf86ce57b06435fc27658a87fbe5a8
DRQE <https://github.com/DRQE/mamimumema>
2022-03-03 14:30:49 +0900

    オートエイム

--- Assets/Scenes/ProtoMain/Scripts/MagicPointer.cs
+++ Assets/Scenes/ProtoMain/Scripts/MagicPointer.cs
@@ -30,12 +30,28 @@ public class MagicPointer : MonoBehaviour
 
         var point = mePos + ((h - Vector3.Dot(floorNormal,mePos)) / (Vector3.Dot(floorNormal, meVec))) * meVec;
 
-        //pointer.transform.position = point;
-        pointer.transform.position = EnemyMove.instance.enemyPosition;
-
-        if(EnemyMove.instance.enemyPosition == null)
+        GameObject mostNearEnemy = null;
+        //敵のなんたら
         {
-            pointer.transform.position = EnemyMove.instance.enemyPosition;
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+
+            float uenoPos = 9999;
+            foreach (GameObject o in enemies)
+            {
+                float dis = Vector3.Distance(transform.position, o.transform.position);
+                if (dis < uenoPos)
+                {
+                    uenoPos = dis;
+                    mostNearEnemy = o;
+                }
+            }
         }
+
+        //pointer.transform.position = point;
+        //pointer.transform.position = EnemyMove.instance.enemyPosition;
+        pointer.transform.position = mostNearEnemy.transform.position;
+
+        transform.LookAt(pointer.transform);
     }
 }
