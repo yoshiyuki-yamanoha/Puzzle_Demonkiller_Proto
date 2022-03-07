65de93d08cda41b546ea4d705a574d0dde598fd9
hidetoo <hideto-oshiro-19h@stu.kbc.ac.jp>
2022-03-07 14:43:08 +0900

    ホーミング修正

--- Assets/Characters/Magic/Scripts/Magichoming.cs
+++ Assets/Characters/Magic/Scripts/Magichoming.cs
@@ -28,36 +28,30 @@ public class Magichoming : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
+        Vector3 Target;
         //transform.position = Vector3.Lerp(transform.position, TargetPos, speed);
 
-        transform.LookAt(TargetObject.transform);
+        //ターゲットの方向に向く
+        if (combo > 5)
+        {
+            //動いている敵の座標を向く
+            transform.LookAt(TargetObject.transform.position);
+            Target = TargetObject.transform.position;
+        }
+        else
+        {
+            //呼び出されたときの敵の座標を向く
+            transform.LookAt(TargetPos);
+            Target = TargetPos;
+        }
 
+        //Zに向かって移動
         transform.position += transform.rotation * new Vector3(0, 0, 1.0f) * speed;
 
-        //if (Vector3.Distance(transform.position, TargetPos) < 10.0f)
-        //{
-        //    if (combo > 5)
-        //    {
-        //        GameObject Explo = Instantiate(Exp, transform.position, Quaternion.identity);
-        //        Destroy(gameObject);
-        //        Destroy(Explo, 1.0f);
-        //    }
-        //    else
-        //    {
-        //        GameObject Explo = Instantiate(ExpMini, transform.position, Quaternion.identity);
-        //        Destroy(gameObject);
-        //        Destroy(Explo, 1.0f);
-        //    }
-
-        //}
-    }
-    void OnTriggerEnter(Collider other)
-    {
-        //Enemyタグに当たったらコンボに応じて爆発のEffectを表示
-        if (other.gameObject.tag == "Enemy")
+        //Debug.Log(Vector3.Distance(transform.position, TargetPos));
+        if (Vector3.Distance(transform.position, Target) < 0.5f)
         {
-            if (combo > 5)
+            if (combo > 10)
             {
                 GameObject Explo = Instantiate(Exp, transform.position, Quaternion.identity);
                 Destroy(gameObject);
@@ -69,6 +63,26 @@ public class Magichoming : MonoBehaviour
                 Destroy(gameObject);
                 Destroy(Explo, 1.0f);
             }
+
         }
     }
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    //Enemyタグに当たったらコンボに応じて爆発のEffectを表示
+    //    if (other.gameObject.tag == "Enemy")
+    //    {
+    //        if (combo > 5)
+    //        {
+    //            GameObject Explo = Instantiate(Exp, transform.position, Quaternion.identity);
+    //            Destroy(gameObject);
+    //            Destroy(Explo, 1.0f);
+    //        }
+    //        else
+    //        {
+    //            GameObject Explo = Instantiate(ExpMini, transform.position, Quaternion.identity);
+    //            Destroy(gameObject);
+    //            Destroy(Explo, 1.0f);
+    //        }
+    //    }
+    //}
 }
