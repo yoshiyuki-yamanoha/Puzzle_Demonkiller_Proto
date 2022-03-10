de1fa45f9781950fe963caceaaf2012419ed293a
ZAHA61 <65996616+ZAHA61@users.noreply.github.com>
2022-03-10 12:37:24 +0900

    //newパック

---
+++ Assets/NewPack/Barricade/Script/Barricade.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barricade : MonoBehaviour
+{
+    [SerializeField] Material material_color = default;
+    Color init_color = default;//初期カラー
+    float hp = 0;
+    [SerializeField] float maxhp = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = maxhp;
+        init_color = material_color.color;//初期カラー保存
+    }
+
+    //barricadematerialカラー変更
+    public void ColorChange()
+    {
+        float damageColor = hp / maxhp;
+        material_color.color = new Color(damageColor, damageColor, damageColor, 1);
+    }
+
+    //barricade耐久値
+    public void Damage(float attack)
+    {
+        hp -= attack;
+        if (hp <= 0)
+        {
+            BarricadeDestroy();
+        }
+    }
+
+    //barricade消す。
+    void BarricadeDestroy()
+    {
+        Destroy(this.gameObject);//自分自身を消す。
+    }
+}
