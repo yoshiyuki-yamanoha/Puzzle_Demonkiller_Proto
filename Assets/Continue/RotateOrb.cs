6104f29c24f19eeb2b5942c8742ef3942efbee7b
Mr-JK4649 <ryouga-shimoji-19h@stu.kbc.ac.jp>
2022-03-31 12:52:03 +0900

    なんかすげぇ

---
+++ Assets/Continue/RotateOrb.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOrb : MonoBehaviour
+{
+    //回る速度倍率　レベルによって変わる
+    public float rotateSpeedMultiply = 1;
+    float rotateSpeed = 1f;
+
+    Vector3 rotateVec;
+
+    private void Start()
+    {
+        rotateVec = Vector3.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        float multi = (rotateSpeedMultiply - 1) * (rotateSpeedMultiply - 1);
+        rotateVec.y = rotateSpeed * multi;
+
+        transform.Rotate(rotateVec);
+    }
+}
