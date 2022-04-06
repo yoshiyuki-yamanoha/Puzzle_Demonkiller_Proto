131dcf2174c81a188c759f9969ee51d9c447a31a
yumyum48 <puxipu939@gmail.com>
2022-04-06 16:10:35 +0900

    Puzzleターンが終了した際のアニメーションの作成

--- Assets/yuma/Script/PuzzleTurnEndAnim.cs
+++ Assets/yuma/Script/PuzzleTurnEndAnim.cs
@@ -9,72 +9,93 @@ public class PuzzleTurnEndAnim : TrunManager
     // アニメーション用の変数
     float startCoolTime;    // アニメーションに入る際少し硬直させる
     [SerializeField] float rotateSpeed; //魔法陣の回転速度
-    float alpha;    // 小さい魔法陣の透明度
+    float inAlpha;    // 小さい魔法陣の透明度
+    float outAlpha;    // 大きい魔法陣の透明度
 
     TrunManager turnMgr;
 
     // アニメーションで使用するGameObject
     [SerializeField] GameObject inCircle;
     [SerializeField] GameObject outCircle;
+    [SerializeField] GameObject[] miniCircles = new GameObject[5];      // 小さい魔法陣の情報
     [SerializeField] MeshRenderer[] circleMesh = new MeshRenderer[5];   // 小さい魔法陣の透明度を変更する変数
     [SerializeField] MeshRenderer outCircleMesh;   // 大きい魔法陣の透明度を変更する変数
 
     // バックアップ用のGameObject
-    GameObject buf_inCircle;
-    GameObject buf_outCircle;
+    Quaternion buf_inCircleRotate;
+    Quaternion buf_outCircleRotate;
 
     void Init()
     {
-        alpha = 1.0f;
+        outAlpha = 0.5f;
+        inAlpha = 1.0f;
         startCoolTime = 60.0f;
         rotateSpeed = 5.0f;
+        inCircle.transform.rotation = buf_inCircleRotate;
+        outCircle.transform.rotation = buf_outCircleRotate;
 
     }
     // Start is called before the first frame update
     void Start()
     {
-        Init();
         trunEndFlg = false;
         turnMgr = GameObject.Find("TrunManager").GetComponent<TrunManager>();
-        buf_inCircle = inCircle;
-        buf_outCircle = outCircle;
+        buf_inCircleRotate = inCircle.transform.rotation;
+        buf_outCircleRotate = outCircle.transform.rotation;
+        Init();
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
+        if(turnMgr.GetTrunPhase() == TrunPhase.Puzzle)
+        {
+            inCircle.SetActive(true);
+            outCircle.SetActive(true);
+            
+        }
         if (GetPuzzleTurnEndFlg())
         {
             //inCircle.transform.Rotate(0.0f, 0.0f, rotateSpeed);
             //outCircle.transform.Rotate(-rotateSpeed, 0.0f, 0.0f);
 
             //Debug.Log("OK");
-
+            for (int i = 0; i < 5; i++)
+            {
+                miniCircles[i].GetComponent<LineRenderer>().enabled = false;
+            }
             if (startCoolTime-- < 0.0f)
             {
+                
                 startCoolTime = 0;
                 inCircle.transform.Rotate(0.0f, 0.0f, rotateSpeed + 5.0f);
                 outCircle.transform.Rotate(0.0f,rotateSpeed, 0.0f);
 
-                if(alpha <= 0.0f)
+                if(inAlpha <= 0.0f)
                 {
-                    alpha = 0.0f;
+                    inAlpha = 0.0f;
                     turnMgr.SetTrunPhase(TrunPhase.MagicAttack);
                     SetPuzzleTurnEndFlg(false);
                     Init();
-
+                    inCircle.SetActive(false);
+                    outCircle.SetActive(false);
+                    for (int i = 0; i < 5; i++)
+                    {
+                        miniCircles[i].GetComponent<LineRenderer>().enabled = true;
+                    }
                 }
                 else
                 {
-                    alpha -= 0.03f;
+                    inAlpha -= 0.03f;
+                    outAlpha -= 0.03f;
                 }
                 
                 for(int i=0;i<5; i++)
                 {
-                    circleMesh[i].material.color = new Color(circleMesh[i].material.color.r, circleMesh[i].material.color.g, circleMesh[i].material.color.b, alpha);
+                    circleMesh[i].material.color = new Color(circleMesh[i].material.color.r, circleMesh[i].material.color.g, circleMesh[i].material.color.b, inAlpha);
                 }
 
-                outCircleMesh.material.color = new Color(outCircleMesh.material.color.r, outCircleMesh.material.color.g, outCircleMesh.material.color.b, alpha);
+                outCircleMesh.material.color = new Color(outCircleMesh.material.color.r, outCircleMesh.material.color.g, outCircleMesh.material.color.b, outAlpha);
             }
         }
     }
