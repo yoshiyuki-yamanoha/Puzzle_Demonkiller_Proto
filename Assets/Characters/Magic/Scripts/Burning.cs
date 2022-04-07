50f4d7c66fe10f076eb19b1677ed093327344a17
DRQE <https://github.com/DRQE/mamimumema>
2022-04-07 15:30:31 +0900

    仮の五角形

---
+++ Assets/Characters/Magic/Scripts/Burning.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burning : MonoBehaviour
+{
+    [SerializeField] private GameObject Burning_model;
+
+    private float Damege = 2;
+
+
+    private void Start()
+    {
+        Instantiate(Burning_model, transform.position, Quaternion.identity);
+    }
+
+    public void Burning_damege()
+    {
+        this.GetComponent<Enemy>().Damage(Damege);
+    }
+}
---
+++ Assets/Characters/Magic/Scripts/Pentagonfire.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pentagonfire : MonoBehaviour
+{
+    [SerializeField] GameObject fireWall;
+
+    private float level;
+
+    private bool EnemyInvasion;//適当変数
+    private bool F_magicStartUp;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        CheckEnemy();
+
+        //if ((!F_magicStartUp) && (EnemyInvasion))
+        //{
+        //    //炎の壁の処理
+        //    //P_Fire();
+        //}
+    }
+
+    public void CheckEnemy()
+    {
+        //if(えねみーの座標が魔法陣と一緒なら){
+        //EnemyInvasion = true;
+        //}
+        //if(えねみーの座標が炎の壁と一緒なら){
+        //this.gameObject.AddComponent<Burning>();
+        //}
+    }
+    public void P_Fire(GameObject tage)
+    {
+        Instantiate(fireWall, tage.transform.position, Quaternion.identity);
+
+        F_magicStartUp = true;
+    }
+}
