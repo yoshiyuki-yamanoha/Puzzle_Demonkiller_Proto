5ff6608fda4709f16573571e060c35bede2e5065
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2022-04-07 16:21:28 +0900

    二種類の魔法を作成した

--- Assets/Characters/Magic/Scripts/Pentagonfire.cs
+++ Assets/Characters/Magic/Scripts/Pentagonfire.cs
@@ -10,31 +10,6 @@ public class Pentagonfire : MonoBehaviour
 
     private bool EnemyInvasion;//適当変数
     private bool F_magicStartUp;
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        CheckEnemy();
-
-        //if ((!F_magicStartUp) && (EnemyInvasion))
-        //{
-        //    //炎の壁の処理
-        //    //P_Fire();
-        //}
-    }
-
-    public void CheckEnemy()
-    {
-        //if(えねみーの座標が魔法陣と一緒なら){
-        //EnemyInvasion = true;
-        //}
-        //if(えねみーの座標が炎の壁と一緒なら){
-        //this.gameObject.AddComponent<Burning>();
-        //}
-    }
     public void P_Fire(GameObject tage)
     {
         Instantiate(fireWall, tage.transform.position, Quaternion.identity);
---
+++ Assets/Shun'sFolder/Star_Fire.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star_Fire : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject Magic_Obj;
+    private int Level = 0;
+    private GameObject tage;
+    private float damege_num = 2.0f;
+
+    private bool TrunInit;      //ターンが切り替わったタイミング用
+
+
+    void Start()
+    {
+        TrunInit = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void set_Init(int _Level, GameObject _tage)
+    {
+        Level = _Level;
+        tage = _tage;
+    }
+
+    public void Magic_Create(int _Level, GameObject[] _tage)
+    {
+        Level = _Level;
+        GameObject[] tages = _tage;
+
+        foreach(GameObject tage in tages)
+        {
+            GameObject Magic = Instantiate(Magic_Obj, tage.transform.position, transform.rotation,transform);
+        }
+    }
+
+    private void ene_Damege(GameObject ene)
+    {
+        Enemy e = ene.GetComponent<Enemy>();
+        if (e != null)
+        {
+            e.Damage(damege_num);
+        }
+
+    }
+}
