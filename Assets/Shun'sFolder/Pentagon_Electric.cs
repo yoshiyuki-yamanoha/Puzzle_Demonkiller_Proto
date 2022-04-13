57f671ee0a6a833ae4be109d0e9bf903f104b5ff
Shun_Nakada <shun-nakada-19h@stu.kbc.ac.jp>
2022-04-13 17:07:31 +0900

    五角形の雷と五芒星の氷の作成 撃つのは未作成

---
+++ Assets/Shun'sFolder/Pentagon_Electric.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pentagon_Electric : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject Electric_Obj;
+
+    [SerializeField, Range(1, 3)] int test_Level = 1;
+    [SerializeField, Range(0, 10)] int cre_X = 0;
+    [SerializeField, Range(0, 14)] int cre_Y = 0;
+
+
+    
+    [SerializeField] private GameObject Stage_mass;
+
+    private int Lev = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GameObject cre_pos = Stage_mass.transform.GetChild(cre_X).GetChild(cre_Y).gameObject;
+            Create_turret(cre_pos, test_Level);
+        }
+    }
+
+    public void Create_turret(GameObject tage, int Lev)
+    {
+        Vector3 cre_pos = tage.transform.position;
+        //タレットの生成位置の補正
+        cre_pos.z -= 2.0f;
+
+        GameObject obj = Instantiate(Electric_Obj, cre_pos, Quaternion.identity);
+
+        obj.GetComponent<Ele_tur_Attack>().Set_Init(Lev, Get_Map_Pos(tage),Stage_mass);
+    }
+
+    private Vector2 Get_Map_Pos(GameObject _tage)
+    {
+        Vector2 pos = new Vector2(0, 0);
+
+        //for(int i = 0; i < _tage.transform.root.childCount; i++)
+        //{
+        //    if(_tage.name == _tage.transform.root.GetChild(i).name)
+        //    {
+        //        pos.x = i;
+        //        break;
+        //    }
+        //}
+
+        //for (int i = 0; i < _tage.transform.root.childCount; i++)
+        //{
+        //    if (_tage.name == _tage.transform.root.GetChild((int)pos.x).gameObject.name)
+        //    {
+        //        pos.y = i;
+        //        break;
+        //    }
+        //}
+        pos = new Vector2(cre_X, cre_Y);
+
+        return pos;
+    }
+}
---
+++ Assets/Shun'sFolder/Star_Ice.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star_Ice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]private GameObject Ice;
+    [SerializeField]private GameObject Ice_efe;
+    [SerializeField]private GameObject Stage_mass;
+
+    private float upSpeed = 10.0f;
+    private float Max_y = 0.8f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Create_IceBergs(4);
+        }
+        child_Move();
+    }
+
+    public void Create_IceBergs(int num)
+    {
+        Transform parent_obj = Stage_mass.transform.GetChild(num);
+
+        for(int i = 0; i < parent_obj.childCount; i++)
+        {
+            StartCoroutine(cre_ice(num, i));
+        }
+    }
+
+    IEnumerator cre_ice(int child, int g_child)
+    {
+        float delay = g_child * 0.1f;
+        yield return new WaitForSeconds(delay);
+        int num = Stage_mass.transform.GetChild(child).childCount - g_child;
+        Vector3 crePos = new Vector3(Stage_mass.transform.GetChild(child).GetChild(num).position.x,
+                                        Stage_mass.transform.GetChild(child).GetChild(num).position.y - 2.0f,
+                                           Stage_mass.transform.GetChild(child).GetChild(num).position.z);
+
+
+        GameObject IceChild = Instantiate(Ice, crePos, Quaternion.identity, this.transform);
+        GameObject Iceefe = Instantiate(Ice_efe, Stage_mass.transform.GetChild(child).GetChild(num).position, Quaternion.identity);
+        IceChild.transform.localScale = new Vector3(2.5f, 1.0f, 2.5f);
+        Iceefe.transform.localScale = new Vector3(1f, 1.5f, 1f);
+        Destroy(IceChild, 3.0f);
+        Destroy(Iceefe, 1.5f);
+    }
+
+    private void child_Move()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            Vector3 pos = transform.GetChild(i).transform.localPosition;
+            if (Max_y > pos.y)
+            {
+                pos.y += upSpeed * Time.deltaTime;
+
+                transform.GetChild(i).transform.localPosition = pos;
+            }
+        }
+    }
+}
