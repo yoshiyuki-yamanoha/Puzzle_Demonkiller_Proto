5e8c74ff04eba643184bc745868b1f22b4d70568
yumyum48 <puxipu939@gmail.com>
2022-04-16 19:37:24 +0900

    タイトルシーンとセレクトシーンのカーソル移動の処理を修正

---
+++ Assets/Scenes/Title/Scripts/MenuControll.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControll : MonoBehaviour
+{
+
+    //class ControllInfo
+    //{
+    //    float inputY;
+
+    //}
+    public enum UpDown
+    {
+        UP,
+        DOWN,
+        NO = 99
+    }
+
+    float selectCoolTime;
+    const float SELECT_COOLTIME_MAX = 10.0f;// 選択カーソルが長押ししていても一瞬その場所で止まる最大時間
+
+    private void Start()
+    {
+        selectCoolTime = SELECT_COOLTIME_MAX;
+    }
+
+    public int GetUpDown()
+    {
+        selectCoolTime--;// クールタイムを起動
+        float inputInfo = Input.GetAxis("UPDOWN");
+        if (inputInfo == 0.0f)
+        {
+            selectCoolTime = 0;
+        }
+
+        if(selectCoolTime <= 0.0f)
+        {
+
+            selectCoolTime = 0;   // クールタイムの固定
+            if(inputInfo >= 0.1f)
+            {
+                selectCoolTime = SELECT_COOLTIME_MAX;
+                return ((int)UpDown.UP);
+            }else if (inputInfo <= -0.1f)
+            {
+                selectCoolTime = SELECT_COOLTIME_MAX;
+                return ((int)UpDown.DOWN);
+            }
+
+        }
+
+        return ((int)UpDown.NO);
+    }
+}
--- Assets/Scenes/Title/Scripts/TitleMgr.cs
+++ Assets/Scenes/Title/Scripts/TitleMgr.cs
@@ -15,6 +15,8 @@ public class TitleMgr : MonoBehaviour
     const float SELECT_COOLTIME_MAX = 10.0f;// 選択カーソルが長押ししていても一瞬その場所で止まる最大時間
     Vector3[] selectMenuPos = new Vector3[2];
     [SerializeField] Transform cursor;
+
+    MenuControll menuControll;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,38 +24,58 @@ public class TitleMgr : MonoBehaviour
         selectCoolTime = 0.0f;
         selectMenuPos[0] = new Vector3(4.98f, -57.0f, 0.0f);
         selectMenuPos[1] = new Vector3(4.98f, -102.6f, 0.0f);
+        this.gameObject.AddComponent<MenuControll>();
+        menuControll = this.GetComponent<MenuControll>();
     }
 
     // Update is called once per frame
-    private void Update() {
-
-        selectCoolTime--;// クールタイムを起動
-        if (Input.GetAxis("UPDOWN") <= 0.9f && Input.GetAxis("UPDOWN") >= 0.0f) {
-            selectCoolTime = 0;
-        }
-        if (selectCoolTime <= 0.0) {
-            float inputInfo = Input.GetAxis("UPDOWN");
-            selectCoolTime = SELECT_COOLTIME_MAX;   // クールタイムの初期化
-            if (Input.GetAxis("UPDOWN") > 0.9f || Input.GetAxis("UPDOWN") < 0.0f) {
-                if(selecter == Select.Start) {
-                    selecter = Select.Quit;
-                }else {
-                    selecter = Select.Start;
-                }
+    private void Update()
+    {
+        
+        if(menuControll.GetUpDown() != 99)
+        {
+            if (selecter == Select.Start)
+            {
+                selecter = Select.Quit;
+            }
+            else
+            {
+                selecter = Select.Start;
             }
         }
 
-        switch (selecter) {
+        switch (selecter)
+        {
             case Select.Start: cursor.localPosition = selectMenuPos[0]; break;
             case Select.Quit: cursor.localPosition = selectMenuPos[1]; break;
         }
 
         // 選択しているStageによって飛ばすシーンを変える
-        if (Input.GetButtonDown("Fire1")) {
-            switch (selecter) {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            switch (selecter)
+            {
                 case Select.Start: GameMgr.Instance.GotoSelectScene(); break;
                 case Select.Quit: GameMgr.Instance.GotoQuit(); break;
             }
         }
+
+        //selectCoolTime--;// クールタイムを起動
+        //if (Input.GetAxis("UPDOWN") <= 0.9f && Input.GetAxis("UPDOWN") >= 0.0f) {
+        //    selectCoolTime = 0;
+        //}
+        //if (selectCoolTime <= 0.0) {
+        //    float inputInfo = Input.GetAxis("UPDOWN");
+        //    selectCoolTime = SELECT_COOLTIME_MAX;   // クールタイムの初期化
+        //    if (Input.GetAxis("UPDOWN") > 0.9f || Input.GetAxis("UPDOWN") < 0.0f) {
+        //        if(selecter == Select.Start) {
+        //            selecter = Select.Quit;
+        //        }else {
+        //            selecter = Select.Start;
+        //        }
+        //    }
+        //}
+
+
     }
 }
