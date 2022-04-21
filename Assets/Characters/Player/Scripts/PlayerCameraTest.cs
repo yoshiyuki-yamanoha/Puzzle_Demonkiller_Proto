64bc2e4099c8a855f33fd76317047d96844e494d
hidetoo <hideto-oshiro-19h@stu.kbc.ac.jp>
2022-04-21 12:35:58 +0900

    魔法を撃つときのカメラが動くようになりましたこれはほんと

--- Assets/Characters/Player/Scripts/PlayerCameraTest.cs
+++ Assets/Characters/Player/Scripts/PlayerCameraTest.cs
@@ -53,7 +53,7 @@ public class PlayerCameraTest : TrunManager
         (soeX, soeY) = selector.GetMAgicMassSelector();
         selepos = selector.GetGameObjectOfSpecifiedMass(soeX, soeY);
         start = subCamera.transform.position;
-        target = new Vector3(/*selector.transform.position.x*/25, selepos.transform.position.y + 25, selepos.transform.position.z - 27);
+        target = new Vector3(selepos.transform.position.x, selepos.transform.position.y + 25, selepos.transform.position.z - 27);
         MagicCameraOn();
         MagicCameraMove();
 
