8ef76585ad2d6bd0a2b6502a60eca652e7bf27c7
ellio001 <11806042@stu.kbc.ac.jp>
2022-04-25 17:00:30 +0900

    Gate用のスクリプトの作成

---
+++ Assets/yuma/Script/EnemySpawnGate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnGate : MonoBehaviour
+{
+    [SerializeField] int enemy_Max;
+    [SerializeField] private Transform[] enePos = null;
+
+    public void Init(int enemyMax) {
+        enePos = new Transform[enemyMax];
+    }
+
+    
+}
