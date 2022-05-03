25b704aeb99f92b62aba48e3d1ce7d70a1933324
DRQE <https://github.com/DRQE/mamimumema>
2022-05-03 13:34:23 +0900

    age

---
+++ Assets/Characters/Magic/Scripts/PentaIceWall.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PentaIceWall : MonoBehaviour
+{
+    bool Is_once, afterOneTurn;
+    int LifeTrun;
+    TrunManager tm;
+    private void Start()
+    {
+        Is_once = true;
+        tm = GameObject.Find("TrunManager").GetComponent<TrunManager>();
+        LifeTrun = 3;
+    }
+    private void FixedUpdate()
+    {
+        if (tm.GetTrunPhase() == TrunManager.TrunPhase.Enemy && Is_once)
+        {
+            afterOneTurn = true;
+            Is_once = false;
+        }
+        else if (tm.GetTrunPhase() == TrunManager.TrunPhase.MagicAttack)
+        {
+            if (!Is_once)
+            {
+                Is_once = true;
+            }
+        }
+
+        if (afterOneTurn)
+        {
+            Debug.Log("START" + LifeTrun);
+            LifeTrun--;
+            if (LifeTrun <= 0) Destroy(this.gameObject);
+            afterOneTurn = false;
+        }
+    }
+}
