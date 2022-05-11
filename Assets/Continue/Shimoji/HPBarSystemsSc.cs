0ca985b078212baf8f94985281f862a7d030127c
Kogorou <bio0730r@gmail.com>
2022-05-11 15:43:44 +0900

    HPゲージ

---
+++ Assets/Continue/Shimoji/HPBarSystemsSc.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBarSystemsSc : MonoBehaviour
+{
+    [SerializeField] Transform startPosObj;
+    [SerializeField] GameObject parts;
+    [SerializeField] CoreBase.Core _core;
+
+    GameObject[] partsList;
+
+    //前フレームのコアのHP
+    int oldCoreHp;
+
+    void Start() {
+        for (int i = 0; i < _core.max_hp; i++)
+        {
+            partsList[i] = Instantiate(parts, startPosObj.position, Quaternion.identity);
+        }
+    }
+
+    void FixedUpdate() {
+        int max = _core.max_hp;
+        int hp = _core.hp;
+
+        if (oldCoreHp != hp) {
+
+            for (int i = 0; i < max; i++) {
+                Image img = partsList[i].transform.GetChild(1).GetComponent<Image>();
+                if (i < hp) img.enabled = true;
+                else img.enabled = false;
+            }
+
+            
+        }
+
+        oldCoreHp = hp;
+    }
+}
