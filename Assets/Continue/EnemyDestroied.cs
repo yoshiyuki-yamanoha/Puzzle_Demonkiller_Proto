f3b77b3a7aa314ed4e7dbaee0f315e87f9be9ac5
ZAHA61 <65996616+ZAHA61@users.noreply.github.com>
2022-05-13 18:01:15 +0900

    幻のスクリプト　(コメントアウト)

--- Assets/Continue/EnemyDestroied.cs
+++ Assets/Continue/EnemyDestroied.cs
@@ -1,28 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class EnemyDestroied : MonoBehaviour
-{
-    GenerationEnemy ge;
-    UltManager ultmanager;
+//public class EnemyDestroied : MonoBehaviour
+//{
+//    GenerationEnemy ge;
+//    UltManager ultmanager;
 
-    private void Start()
-    {
-        ultmanager = GameObject.Find("GameObject").GetComponent<UltManager>();
-        ge = GameObject.Find("Sponer").GetComponent<GenerationEnemy>();
-    }
+//    private void Start()
+//    {
+//        ultmanager = GameObject.Find("GameObject").GetComponent<UltManager>();
+//        ge = GameObject.Find("Sponer").GetComponent<GenerationEnemy>();
+//    }
 
-    void OnTriggerEnter(Collider other)
-    {
-        //Enemyタグに当たったらコンボに応じて爆発のEffectを表示
-        if (other.gameObject.tag == "MarkedEnemy")
-        {
-            ultmanager = GameObject.Find("GameObject").GetComponent<UltManager>();
-            ultmanager.ultChage();
-            ge = GameObject.Find("Sponer").GetComponent<GenerationEnemy>();
-            Destroy(other.gameObject);
-            ge.initflg = true;
-        }
-    }
-}
+//    private void FixedUpdate()
+//    {
+//        Debug.Log("使われてます。");
+//    }
+
+//    void OnTriggerEnter(Collider other)
+//    {
+//        //Enemyタグに当たったらコンボに応じて爆発のEffectを表示
+//        if (other.gameObject.tag == "MarkedEnemy")
+//        {
+//            ultmanager = GameObject.Find("GameObject").GetComponent<UltManager>();
+//            ultmanager.ultChage();
+//            ge = GameObject.Find("Sponer").GetComponent<GenerationEnemy>();
+//            Destroy(other.gameObject);
+//            ge.initflg = true;
+//        }
+//    }
+//}
