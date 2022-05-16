9f720d441e7d5a9adad414442346eb554bb00ae3
ZAHA61 <65996616+ZAHA61@users.noreply.github.com>
2022-05-16 11:56:19 +0900

    //敵のマスに色オブジェクトを追加した。

---
+++ Assets/ZAHA/EnemyMass/Scripts/FixedRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("子のscale" + transform.localScale);
+        //Debug.Log("親のscale" + transform.root.localScale);
+        //Vector3 scalesize = new Vector3(
+        //        transform.localScale.x / transform.root.localScale.x,
+        //        transform.localScale.y / transform.root.localScale.y,
+        //        transform.localScale.z / transform.root.localScale.z
+        //);
+
+        //transform.localScale = scalesize;
+
+        //Debug.Log("scaleサイズ " + scalesize);
+    }
+
+    private void FixedUpdate()
+    {
+        gameObject.transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+    }
+}
