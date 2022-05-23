8f46dd1d5ad3596c95a546789ac0ec1ec8a08f38
ZAHA61 <65996616+ZAHA61@users.noreply.github.com>
2022-05-23 16:31:31 +0900

    おっけーmaterialチェンジあったよー

---
+++ Assets/MaterialChange.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChange : MonoBehaviour
+{
+    [SerializeField] AnimEvent anim_event;
+    public Material change_material;
+    Material material;
+    Renderer my_renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetMaterial();
+        //SetMaterial(change_material);
+    }
+
+    void GetMaterial()
+    {
+        material = transform.gameObject.GetComponent<Renderer>().material;
+    }
+
+    public void SetMaterial(Material material)
+    {
+        my_renderer = transform.gameObject.GetComponent<Renderer>();
+        my_renderer.material = material;
+    }
+
+    public void IceBreakMaterial()
+    {
+        float des = my_renderer.material.GetFloat("_Destruction");//materialの値を取得
+
+        des += Time.deltaTime;//分解materialの値を生成
+
+        if (des >= 0.35f) { 
+            des = 0f;
+            anim_event.FlameBreakIceOff(); //氷を壊すフラグをオフ
+            SetMaterial(material);//materialを戻す。
+            anim_event.Ice_star();
+        }
+
+        my_renderer.material.SetFloat("_Destruction", des);//分解materialの値を更新
+    }
+
+    private void FixedUpdate()
+    {
+        if (anim_event.GetFlameIcebreak())
+        {
+            IceBreakMaterial();
+        }
+    }
+}
---
+++ Assets/Material_Ice.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Material_Ice : MonoBehaviour
+{
+    Material ice_material;
+    float des;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ice_material = transform.gameObject.GetComponent<Renderer>().material;
+        des = ice_material.GetFloat("_Destruction");
+        Debug.Log("アイスmaterialの数値" + des);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+
+    }
+}
