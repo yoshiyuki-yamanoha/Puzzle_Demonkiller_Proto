06f0f626ceecb2299097a7234da488f0f4158ce0
hidetoo <hideto-oshiro-19h@stu.kbc.ac.jp>
2022-05-24 11:11:41 +0900

    上げ忘れ

---
+++ Assets/Characters/Magic/Scripts/MagicCircleColorChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MagicCircleColorChange : MonoBehaviour
+{
+    Image magicCicle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        magicCicle = gameObject.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.transform transform- 
+    }
+
+    public void CircleColorChange(Color c)
+    {
+        magicCicle.color = c;
+    }
+}
